f0cad9d00019e7019dd85bf35af0b11a6ba4fb47
hanjiaming <47519540+guomaimang@users.noreply.github.com>
2024-05-15 14:13:20 +0800

    init

---
+++ src/main/java/tech/hirsun/eslogistic/EsLogisticApplication.java
@@ -0,0 +1,13 @@
+package tech.hirsun.eslogistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsLogisticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsLogisticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/hirsun/eslogistic/EsLogisticApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.hirsun.eslogistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsLogisticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
